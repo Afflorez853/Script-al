d662a009b3328ec054d8f6779e2511f3a69caf8e
Zeus <andres.felipe.florez62@gmail.com>
2024-02-19 15:20:40 -0500

    Script Second

--- Script-al.sh
+++ Script-al.sh
@@ -6,3 +6,10 @@ read -p "Carpeta_Script" folder_name
 mkdir "$foldegit star_name"
 
 
+#Ejecutar un comando en segundo plano 
+
+echo "Executing a command in the background"
+command_to_run="start /b notepad.exe"
+nohup $command_to_run > output.txt 2>&1 &
+echo "The command is running"
+
